9270c00fab1024b6c3eea1e3307587bda67f7477
Matheus da Fonseca <matheus2712@hotmail.com>
2021-08-23 02:02:41 -0300

    Final project

--- src/app/city-hall/display-form-hall/display-form-hall.component.ts
+++ src/app/city-hall/display-form-hall/display-form-hall.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { PoModalAction, PoModalComponent, PoMultiselectOption } from '@po-ui/ng-components';
 import { SocketioService } from 'src/services/web-socket.service';
 
 export interface Form {
@@ -18,7 +20,41 @@ export interface Form {
 })
 export class DisplayFormHallComponent implements OnInit {
 
+  @ViewChild(PoModalComponent, { static: true }) modalEditarForm: PoModalComponent;
+
   @Input() form: Form;
+
+  localActivities: Array<PoMultiselectOption> = [
+    { value: 'futebol', label: 'Futebol' },
+    { value: 'ginastica', label: 'Ginástica' },
+    { value: 'basquete', label: 'Basquete' },
+    { value: 'outros', label: 'Outros' }
+  ];
+
+  primaryActionEditarForm: PoModalAction = {
+    action: () => {
+      this.modifyForm();
+      this.modalEditarForm.close();
+    },
+    label: 'Confirmar'
+  };
+
+  secondaryActionEditarForm: PoModalAction = {
+    action: () => {
+      this.modalEditarForm.close();
+    },
+    label: 'Cancelar'
+  };
+
+  openModal() {
+    this.editCitizenForm.controls['fullName'].patchValue(this.form.fullName);
+    this.editCitizenForm.controls['mailAddress'].patchValue(this.form.mailAddress);
+    this.editCitizenForm.controls['localName'].patchValue(this.form.localName);
+    this.editCitizenForm.controls['value'].patchValue(this.form.value);
+    this.editCitizenForm.controls['localAddress'].patchValue(this.form.localAddress);
+    this.editCitizenForm.controls['allowedActivities'].patchValue(this.form.allowedActivities);
+    this.modalEditarForm.open();
+  }
 
   get allowedActivities(): string {
     if(this.form.allowedActivities.length) {
@@ -27,13 +63,32 @@ export class DisplayFormHallComponent implements OnInit {
     return '';
   }
 
-  constructor(private webSocketService: SocketioService) { }
+  constructor(private webSocketService: SocketioService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  editCitizenForm = this.fb.group({
+    fullName: [''],
+    mailAddress: [''],
+    localName: [''],
+    value: [''],
+    localAddress: [''],
+    allowedActivities: ['']
+  });
+
   modifyForm(): void {
-    this.webSocketService.modifyForm(this.form.id, this.form)
+    const formEdited = {
+      id: this.form.id,
+      fullName: this.editCitizenForm.get('fullName').value,
+      mailAddress: this.editCitizenForm.get('mailAddress').value,
+      localName: this.editCitizenForm.get('localName').value,
+      value: this.editCitizenForm.get('value').value,
+      localAddress: this.editCitizenForm.get('localAddress').value,
+      allowedActivities: this.editCitizenForm.get('allowedActivities').value
+    }
+
+    this.webSocketService.modifyForm(this.form.id, formEdited)
   }
 
   // OK
